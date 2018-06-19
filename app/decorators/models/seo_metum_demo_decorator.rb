e0a527e29a5abbc9e2c226a85ce242d3fe574fc9
gloubier <gloubier@o2web.ca>
2018-06-19 09:16:14 -0400

    Set belongs_to to optional

--- app/decorators/models/seo_metum_demo_decorator.rb
+++ app/decorators/models/seo_metum_demo_decorator.rb
@@ -3,5 +3,5 @@ SeoMetum.class_eval do
 
   # Add an association to the Refinery::Image class, so we
   # can take advantage of the magic that the class provides
-  belongs_to :og_image, class_name: '::Refinery::Image'
+  belongs_to :og_image, class_name: '::Refinery::Image', optional: true
 end
